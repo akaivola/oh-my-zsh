3d84bfc39f7f0e425e98234f5f6649e1924c384e
Aki Kaivola <aki.kaivola@gmail.com>
2012-03-04 09:36:07 +0200

    remove sl

---
+++ lib/aliases.zsh
@@ -0,0 +1,25 @@
+# Push and pop directories on directory stack
+alias pu='pushd'
+alias po='popd'
+
+# Basic directory operations
+alias ...='nocorrect cd ../..'
+alias -- -='cd -'
+
+# Super user
+alias _='sudo'
+alias please='sudo'
+
+#alias g='grep -in'
+
+# Show history
+alias history='fc -l 1'
+
+# List direcory contents
+alias lsa='ls -lah'
+alias l='ls -lA1'
+alias ll='ls -l'
+alias la='ls -lA'
+
+alias afind='ack-grep -il'
+
